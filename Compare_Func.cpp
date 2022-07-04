6ade3a0e8c86d79641febd31f06deb665c11a6f4
O-SoleMio <dhxodid0207@naver.com>
2022-07-04 20:54:53 +0900

    #dfs

--- Compare_Func.cpp
+++ Compare_Func.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int cmp(int a)
-{
-	return a += 1;
-}
-
-int main()
-{
-	vector<int> numbers = {0,10,2,3,5,8,9};
-
-	sort(numbers.begin(), numbers.end());
-	
-	for (int i : numbers) cout << i << endl;
-}
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int cmp(int a)
+//{
+//	return a += 1;
+//}
+//
+//int main()
+//{
+//	vector<int> numbers = {0,10,2,3,5,8,9};
+//
+//	sort(numbers.begin(), numbers.end());
+//	
+//	for (int i : numbers) cout << i << endl;
+//}
