e5d49e87174e926a704660b15bb014dd5cdfb58c
O-SoleMio <87802415+O-SoleMio@users.noreply.github.com>
2022-08-04 17:54:15 +0900

    #unordered_map 학습

---
+++ test_map.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+	unordered_map<string, int> waitingNum { {"KIM", 1}, { "BAM",2 }, { "SIM",3 }, { "BONG",4 }, { "SAM",5 } };
+	//unordered_map<string,int>::iterator findName = waitingNum.find("SAM"); 반환형
+	auto findName = waitingNum.find("SAM");
+
+	if (waitingNum.empty())	{cout << "empty" << endl;} // 비어있는지 확인
+	if (waitingNum.end() == waitingNum.begin()) { cout << "empty2" << endl; } // 비어있는지 확인
+
+	if (findName == waitingNum.end())
+	{
+		cout << "not find" << endl;
+	}
+	else
+	{
+		cout << findName->first << " " << findName->second<< endl;
+	}// 결과 SAM 5
+
+	cout << waitingNum.count("KIM");
+}
