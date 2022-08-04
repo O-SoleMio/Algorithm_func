225e55b91985b1276a428751510bb44027434cd7
O-SoleMio <87802415+O-SoleMio@users.noreply.github.com>
2022-08-04 16:00:22 +0900

    #스마트 포인터 연습, 맵 알고리즘

--- Phone_numbers.cpp
+++ Phone_numbers.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
-
-using namespace std;
-
-bool solution(vector<string> phone_book) {
-    bool answer = true;
-
-    unordered_map<string,int> phone_map;
-    pair<string,int> insert_pair;
-    for (int i = 0; i < phone_book.size(); i++)
-    {
-        insert_pair = make_pair(phone_book[i],i);
-        phone_map.insert(insert_pair);        
-    }
-
-    auto f = phone_map.find("12");
-
-    if(f != phone_map.end()) 
-    {
-        cout << f->second << endl;
-    }
-    
-
-    return answer;
-}
-
-int main()
-{
-    vector<string> phone_book = { "12","12","1235","567","88"};
-    cout << solution(phone_book);
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//bool solution(vector<string> phone_book) {
+//    bool answer = true;
+//
+//    unordered_map<string,int> phone_map;
+//    pair<string,int> insert_pair;
+//    for (int i = 0; i < phone_book.size(); i++)
+//    {
+//        insert_pair = make_pair(phone_book[i],i);
+//        phone_map.insert(insert_pair);        
+//    }
+//
+//    auto f = phone_map.find("12");
+//
+//    if(f != phone_map.end()) 
+//    {
+//        cout << f->second << endl;
+//    }
+//    
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    vector<string> phone_book = { "12","12","1235","567","88"};
+//    cout << solution(phone_book);
+//}
---
+++ test_smartPointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	auto pdata = make_unique<int>(10);
+	
+	//auto pdata_two{};
+	cout << *pdata << endl;
+	cout << *pdata << endl;
+	*pdata += 10;	
+	cout << *pdata << endl;	
+}
